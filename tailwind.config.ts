6ad339faa575a49f879ad4de597b7af1b8f3ac1c
Geon <geonulim@gmail.com>
2025-04-12 23:59:39 +0900

    init: 초기 설정

---
+++ tailwind.config.ts
@@ -0,0 +1,58 @@
+/** @type {import('tailwindcss').Config} */
+
+const createPxEntries = (size: number) => {
+  return {
+    0: '0',
+    ...Array.from(Array(size + 1)).reduce((accumulator, _, i) => {
+      return { ...accumulator, [`${i * 4}`]: `${i * 4}px` };
+    }),
+  };
+};
+
+const PX_ENTRIES = createPxEntries(500);
+
+export default {
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    spacing: PX_ENTRIES,
+    fontSize: {
+      10: '10px',
+      12: '12px',
+      14: '14px',
+      16: '16px',
+      18: '18px',
+      20: '20px',
+      24: '24px',
+      28: '28px',
+      32: '32px',
+    },
+    fontWeight: {
+      400: '400',
+      500: '500',
+      600: '600',
+      700: '700',
+      800: '800',
+    },
+    fontFamily: {
+      sans: ['Pretendard', 'Arial'],
+    },
+    borderRadius: {
+      none: '0px',
+      4: '4px',
+      6: '6px',
+      8: '8px',
+      10: '10px',
+      12: '12px',
+      16: '16px',
+      24: '24px',
+      full: '9999px',
+    },
+    zIndex: {
+      base: '1',
+      nav: '2',
+      popup: '999',
+      floating: '1000',
+    },
+  },
+  plugins: [],
+};
