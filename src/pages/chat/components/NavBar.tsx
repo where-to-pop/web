1846cd3f8eff3d80d2b4ccc40c2e04783526f349
Geon <geonulim@gmail.com>
2025-06-20 15:33:22 +0900

    feat: 채팅방 정렬 순서 수정

--- src/pages/chat/components/NavBar.tsx
+++ src/pages/chat/components/NavBar.tsx
@@ -1,5 +1,6 @@
 import { IconAdd } from 'public/icons';
 import { IconHamburger } from 'public/icons';
+import { useMemo } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { Chat } from 'src/types/chat.type';
 
@@ -11,6 +12,7 @@ interface Props {
 
 const NavBar = ({ projectId, chats, currentChatId }: Props) => {
   const navigate = useNavigate();
+  const reversedChats = useMemo(() => [...chats].reverse(), [chats]);
   return (
     <nav className='group relative h-full w-72'>
       <div className='absolute bottom-0 left-0 top-0 z-nav flex w-[70px] flex-col items-center gap-24 border-r border-grey-300/50 bg-grey-200 py-12'>
@@ -27,7 +29,7 @@ const NavBar = ({ projectId, chats, currentChatId }: Props) => {
       <div className='absolute bottom-0 left-[-130px] top-0 z-base w-200 bg-grey-200/95 px-[10px] py-20 transition-all duration-300 group-hover:translate-x-200'>
         <h2 className='pb-8 text-12 font-400 text-grey-500'>채팅방 목록</h2>
         <ul className='h-full overflow-y-auto scrollbar-hide'>
-          {chats.map((chat) => (
+          {reversedChats.map((chat) => (
             <li key={chat.id} className='border-b border-grey-300/60'>
               <button
                 className={`my-12 line-clamp-1 w-full rounded-6 px-4 py-4 text-left text-14 font-500 hover:bg-grey-300/50 ${
